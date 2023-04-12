408976423e1f891b87c6d79c2ad310735da3be06
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-04-12 21:35:05 +0700

    feat: add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{astro}'],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    colors: {
+      'background-site-body': 'hsl(var(--background-site-body))',
+      'color-site-primary': 'hsl(var(--color-site-primary))',
+      'color-site-subtle': 'hsl(var(--color-site-subtle))',
+      'color-site-normal': 'hsl(var(--color-site-normal))',
+      'color-site-emphasis': 'hsl(var(--color-site-emphasis))',
+      'color-site-stress': 'hsl(var(--color-site-stress))',
+    }
+  },
+  plugins: [],
+} satisfies Config
