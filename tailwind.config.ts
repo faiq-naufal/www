7b57c1c1d3d0bcd8a9d5d8e96e8bf919206b0a22
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-05-13 16:03:41 +0700

    feat: update color and add oklch support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ export default {
   },
   theme: {
     colors: {
-      'background-site-body': 'hsl(var(--background-site-body))',
-      'background-site-rectangle': 'hsl(var(--background-site-rectangle))',
-      'color-site-primary': 'hsl(var(--color-site-primary))',
-      'color-site-subtle': 'hsl(var(--color-site-subtle))',
-      'color-site-normal': 'hsl(var(--color-site-normal))',
-      'color-site-emphasis': 'hsl(var(--color-site-emphasis))',
-      'color-site-stress': 'hsl(var(--color-site-stress))',
+      'background-site-body': 'var(--background-site-body)',
+      'background-site-rectangle': 'var(--background-site-rectangle)',
+      'color-site-primary': 'var(--color-site-primary)',
+      'color-site-subtle': 'var(--color-site-subtle)',
+      'color-site-normal': 'var(--color-site-normal)',
+      'color-site-emphasis': 'var(--color-site-emphasis)',
+      'color-site-stress': 'var(--color-site-stress)',
     }
   },
   plugins: [],
