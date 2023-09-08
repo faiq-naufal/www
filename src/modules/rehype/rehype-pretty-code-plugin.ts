7f87546ef08a116d059d6c095fc92a8310ded99d
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-08 20:43:09 +0700

    refactor: fix spacing

--- src/modules/rehype/rehype-pretty-code-plugin.ts
+++ src/modules/rehype/rehype-pretty-code-plugin.ts
@@ -2,22 +2,24 @@ import rehypePrettyCode from 'rehype-pretty-code'
 import type { Options as RehypePrettyCodeOptions } from 'rehype-pretty-code'
 
 export const rehypePrettyCodeOptions: Partial<RehypePrettyCodeOptions> = {
-	theme: {
-		dark: 'vitesse-dark',
-		light: 'vitesse-light',
-	},
-	keepBackground: false,
-	onVisitLine(node) {
-		if (node.children.length === 0) {
-			node.children = [{ type: 'text', value: ' ' }]
-		}
-	},
-	onVisitHighlightedLine(node) {
-		node.properties.className.push('highlighted')
-	},
-	onVisitHighlightedWord(node) {
-		node.properties.className = ['word']
-	},
+  theme: {
+    dark: 'vitesse-dark',
+    light: 'vitesse-light',
+  },
+  keepBackground: false,
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }]
+    }
+  },
+  onVisitHighlightedLine(node) {
+    if (!node || !node.properties || !node.properties.className) return
+    node.properties.className.push('highlighted')
+  },
+  onVisitHighlightedChars(node) {
+    if (!node || !node.properties || !node.properties.className) return
+    node.properties.className = ['word']
+  },
 }
 
 export const rehypePrettyCodePlugin = rehypePrettyCode
