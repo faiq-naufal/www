7bf2f5b03937e8c6a1f30b1d3633dcc42d409de6
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-17 15:50:32 +0700

    feat: critical foft (#15)
    * refactor: uninstall fontaine
    
    * refactor: move font.css to public folder
    
    * feat: add font.css and preload webfonts
    
    * feat: add font loader using critical font approach
    
    * style: update base font family style
    
    * refactor: load fonts when fonts are loaded using js
    
    * feat: don't ignore public folder
    
    * feat: add browser env
    
    * refactor: move font loader to regular script
    
    * refactor: uncomment font-display
    
    * refactor: remove non-critical fonts and add script font loader
    
    * feat: add crossorigin attribute
    
    * refactor: change preload attributes for individual type
    
    * feat: call fontloader on astro:after-swap event
    
    * fix: preload should persist on view transition

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,6 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import partytown from '@astrojs/partytown'
 import mdx from '@astrojs/mdx'
-import { FontaineTransform } from 'fontaine'
 import { remarkReadingTime } from './src/modules/remark/remark-reading-time-plugin'
 import {
   rehypePrettyCodePlugin,
@@ -19,22 +18,6 @@ const { PUBLIC_CANONICAL_ORIGIN } = process.env
 
 export default defineConfig({
   site: PUBLIC_CANONICAL_ORIGIN,
-  vite: {
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: [
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Helvetica Neue',
-          'Segoe UI',
-          'Arial',
-          'Roboto',
-          'Open Sans',
-        ],
-        resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
-      }),
-    ],
-  },
   integrations: [
     partytown({}),
     UnoCSS({
---
+++ public/scripts/font-loader.js
@@ -0,0 +1,25 @@
+function fontLoader() {
+  if (sessionStorage.fontsLoaded) {
+    document.documentElement.classList.add('sans-serif-loaded')
+    document.documentElement.classList.add('monospace-loaded')
+    return
+  }
+
+  if (document.fonts) {
+    Promise.all([
+      document.fonts.load('normal 1em Public Sans Variable'),
+      document.fonts.load('italic 1em Public Sans Variable'),
+    ]).then(() => {
+      document.documentElement.classList.add('sans-serif-loaded')
+
+      document.fonts.load('normal 1em JetBrains Mono Variable').then(() => {
+        document.documentElement.classList.add('monospace-loaded')
+        sessionStorage.fontsLoaded = true
+      })
+    })
+  }
+}
+
+fontLoader()
+
+document.addEventListener('astro:after-swap', fontLoader)
