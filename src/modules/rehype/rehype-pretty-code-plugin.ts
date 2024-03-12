28fcb16b40d6149f5ade09ecb5267d1dd1ffc627
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-03-13 06:13:36 +0700

    refactor: remove unused plugin configuration

--- src/modules/rehype/rehype-pretty-code-plugin.ts
+++ src/modules/rehype/rehype-pretty-code-plugin.ts
@@ -1,27 +1,11 @@
 import rehypePrettyCode from 'rehype-pretty-code'
-import type { Options as RehypePrettyCodeOptions } from 'rehype-pretty-code'
-
-export const rehypePrettyCodeOptions: RehypePrettyCodeOptions = {
-  theme: {
-    dark: 'github-dark',
-    light: 'github-light',
-  },
-  keepBackground: false,
-  onVisitLine(node) {
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }]
-    }
-  },
-  onVisitHighlightedLine(node) {
-    if (!node || !node.properties || !node.properties.className) return
-    node.properties.className.push('highlighted')
-  },
-  onVisitHighlightedChars(node) {
-    if (!node || !node.properties || !node.properties.className) return
-    node.properties.className = ['word']
-  },
-}
 
 export const rehypePrettyCodePlugin = () => {
-  return rehypePrettyCode(rehypePrettyCodeOptions)
+  return rehypePrettyCode({
+    theme: {
+      dark: 'github-dark',
+      light: 'github-light',
+    },
+    keepBackground: false,
+  })
 }
