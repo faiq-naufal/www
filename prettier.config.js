9370341a551c460b93c9246bb9099343231f1e01
faiq-naufal <43695578+faiq-naufal@users.noreply.github.com>
2025-02-22 22:34:26 +0700

    fix(prettier): adjust prettier config to support astro

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,20 @@
 /** @type {import("prettier").Config} */
 const config = {
   printWidth: 100,
-  semi: false,
+  semi: true,
   singleQuote: true,
-  trailingComma: 'es5',
+  trailingComma: 'all',
   tabWidth: 2,
   useTabs: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
 
 export default config;
